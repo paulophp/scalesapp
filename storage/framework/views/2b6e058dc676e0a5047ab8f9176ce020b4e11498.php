6384ae543fefc152b0eb2bd0c0c216bb06ab0067
Paulo Queiroz <paulophp@gmail.com>
2023-01-16 18:45:55 -0300

    Revert "e ae bora programar"
    This reverts commit c89ca69e78436b9dcdd5ec5c81f99f26172a44a4.

---
+++ storage/framework/views/2b6e058dc676e0a5047ab8f9176ce020b4e11498.php
@@ -0,0 +1,41 @@
+
+<!doctype html>
+<html lang="pt-br" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Template para sticky footer, usando Bootstrap.</title>
+
+    <!-- Principal CSS do Bootstrap -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Estilos customizados para esse template -->
+    <link href="/css/sticky-footer.css" rel="stylesheet">
+  </head>
+
+  <body class="d-flex flex-column h-100">
+
+    <!-- Begin page content -->
+    <main role="main" class="flex-shrink-0">
+      <div class="container">
+        <h1 class="mt-5">Restaurantes</h1>
+        <p class="lead">Uma nova solução para gerenciar seu negócio!</p>
+
+        <p>Acesse sua conta fazendo <a href="/login">login</a>.</p>
+        <p>Ou crie uma nova conta em <a href="/register">registrar</a>.</p>
+
+      </div>
+    </main>
+
+    <footer class="footer mt-auto py-3">
+      <div class="container">
+        <span class="text-muted">Restaurantes 2023</span>
+      </div>
+    </footer>
+  </body>
+</html>
+<?php /**PATH /home/user/cloudfox/restaurantes/resources/views/welcome.blade.php ENDPATH**/ ?>
